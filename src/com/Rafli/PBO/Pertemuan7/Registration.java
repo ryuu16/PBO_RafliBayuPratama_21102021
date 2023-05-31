86e912cf51c8d2e1469644e3883d7538b31515e1
Legion <raflibayuu@gmail.com>
2023-05-31 14:08:47 +0700

    Pertemuan 7

---
+++ src/com/Rafli/PBO/Pertemuan7/Registration.java
@@ -0,0 +1,15 @@
+package com.Rafli.PBO.Pertemuan7;
+
+import javax.swing.*;
+
+public class Registration {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JTextField textField3;
+    private JRadioButton lakiLakiRadioButton;
+    private JRadioButton perempuanRadioButton;
+    private JTextArea textArea1;
+    private JButton saveButton;
+    private JButton clearButton;
+}
