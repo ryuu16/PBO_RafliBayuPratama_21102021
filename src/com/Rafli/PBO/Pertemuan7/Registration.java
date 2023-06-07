55994c63d9cd07a16948ea82613a70bf7a30fa24
Legion <raflibayuu@gmail.com>
2023-06-07 14:34:36 +0700

    Pertemuan 7

--- src/com/Rafli/PBO/Pertemuan7/Registration.java
+++ src/com/Rafli/PBO/Pertemuan7/Registration.java
@@ -2,12 +2,13 @@ package com.Rafli.PBO.Pertemuan7;
 
 import javax.swing.*;
 
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Registration extends JFrame {
     private JTextField TxtName;
     private JTextField TxtNIM;
-    private JComboBox comboBox1;
+    private JComboBox CBProdi;
     private JTextField TxtTelp;
     private JRadioButton lakiLakiRadioButton;
     private JRadioButton perempuanRadioButton;
@@ -22,6 +23,38 @@ public class Registration extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.setSize(800,600);
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TxtName.setText(" ");
+                TxtNIM.setText(" ");
+                TxtTelp.setText(" ");
+                TxtAreaAlamat.setText(" ");
+                CBProdi.setSelectedIndex(0);
+                lakiLakiRadioButton.setSelected(false);
+                perempuanRadioButton.setSelected(false);
+            }
+        });
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nama = TxtName.getText();
+                String NIM = TxtNIM.getText();
+                String prodi = (String) CBProdi.getSelectedItem();
+                String telp = TxtTelp.getText();
+                String alamat = TxtAreaAlamat.getText();
+                String JK;
+                if (lakiLakiRadioButton.isSelected()) {
+                    JK = "Laki-Laki";
+                } else if(perempuanRadioButton.isSelected()) {
+                    JK = "Perempuan";
+                } else {
+                    JK = "Tidak Diketahui";
+                }
+                //menampilkan msgbox
+                JOptionPane.showMessageDialog(null, "Data Berhasil Disimpan");
+            }
+        });
     }
 
     public static void main(String[] args) {
