9fe372483d01fbaeec85c3d9e6ff533496f42930
Legion <raflibayuu@gmail.com>
2023-06-07 14:08:36 +0700

    Pertemuan 7

--- src/com/Rafli/PBO/Pertemuan7/Registration.java
+++ src/com/Rafli/PBO/Pertemuan7/Registration.java
@@ -2,14 +2,31 @@ package com.Rafli.PBO.Pertemuan7;
 
 import javax.swing.*;
 
-public class Registration {
-    private JTextField textField1;
-    private JTextField textField2;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class Registration extends JFrame {
+    private JTextField TxtName;
+    private JTextField TxtNIM;
     private JComboBox comboBox1;
-    private JTextField textField3;
+    private JTextField TxtTelp;
     private JRadioButton lakiLakiRadioButton;
     private JRadioButton perempuanRadioButton;
-    private JTextArea textArea1;
+    private JTextArea TxtAreaAlamat;
     private JButton saveButton;
     private JButton clearButton;
+    private JPanel mainPanel;
+
+    public Registration() {
+        super ("Formulir Registrasi");
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.setSize(800,600);
+    }
+
+    public static void main(String[] args) {
+        JFrame mainFrame = new Registration();
+        mainFrame.setVisible(true);
+    }
+
 }
